20d60a322288d2c9a2a532570f9db28641947c33
Jacob <jacob@britecore.com>
2025-02-05 10:07:42 -0600

    Handle Anonymous users in json auth consumer

--- api/funkwhale_api/common/consumers.py
+++ api/funkwhale_api/common/consumers.py
@@ -14,11 +14,17 @@ class JsonAuthConsumer(JsonWebsocketConsumer):
 
     def accept(self):
         super().accept()
-        groups = self.scope["user"].get_channels_groups() + self.groups
+        user = self.scope["user"]
+        if user.is_anonymous:
+            return
+        groups = user.get_channels_groups() + self.groups
         for group in groups:
             channels.group_add(group, self.channel_name)
 
     def disconnect(self, close_code):
-        groups = self.scope["user"].get_channels_groups() + self.groups
+        user = self.scope["user"]
+        if user.is_anonymous:
+            return
+        groups = user.get_channels_groups() + self.groups
         for group in groups:
             channels.group_discard(group, self.channel_name)
