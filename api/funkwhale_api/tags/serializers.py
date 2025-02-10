752f868156e45fa4c4511d2ed852098c5c5e8f90
Jacob <jacob@britecore.com>
2025-02-10 12:35:21 -0600

    Backend updates for track tag editing

--- api/funkwhale_api/tags/serializers.py
+++ api/funkwhale_api/tags/serializers.py
@@ -43,3 +43,41 @@ class TagsListField(serializers.ListField):
         # we ignore any extra tags if the length of the list is higher
         # than our accepted size
         return value[: settings.TAGS_MAX_BY_OBJ]
+
+
+class CategorizedTagsField(serializers.DictField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("child", serializers.ListField(child=TagNameField()))
+        super().__init__(*args, **kwargs)
+
+    def to_internal_value(self, value):
+
+        if isinstance(value, list) and value and isinstance(value[0], dict) and value[0].get("tag"):
+            # This is a list of dictionaries, we can assume the format is correct
+            return value
+
+        if not isinstance(value, dict):
+            raise serializers.ValidationError("Expected a dictionary of category names to tag lists")
+
+        validated_data = {}
+        for category_name, tags in value.items():
+            try:
+                category = models.TagCategory.objects.get(name=category_name)
+            except models.TagCategory.DoesNotExist:
+                raise serializers.ValidationError(f"Tag category '{category_name}' does not exist")
+
+            # Validate tags list for this category
+            if len(tags) > category.max_tags:
+                raise serializers.ValidationError(
+                    f"Category '{category_name}' allows maximum {category.max_tags} tags"
+                )
+
+            # Store validated tags with their category
+            validated_tags = super().to_internal_value({category_name: tags})[category_name]
+            validated_data[category_name] = validated_tags
+
+        return [
+            {"tag": tag, "tag_category": category}
+            for category, tags in validated_data.items()
+            for tag in tags
+        ]
