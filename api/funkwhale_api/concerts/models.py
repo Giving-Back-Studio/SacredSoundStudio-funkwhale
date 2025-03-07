20059d40c6df4c347dda8be8f8dc35ab43d25029
Jacob <jacob@britecore.com>
2025-03-07 10:28:25 -0600

    Add artist and permissions to concert API

--- api/funkwhale_api/concerts/models.py
+++ api/funkwhale_api/concerts/models.py
@@ -4,8 +4,12 @@ class Concert(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     cover = models.ImageField(upload_to="concert_covers/")
+
+    artist = models.ForeignKey('music.Artist', on_delete=models.CASCADE, blank=True, null=True)
+
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+
     mux_live_stream_id = models.CharField(max_length=100, blank=True, null=True)
     mux_playback_id = models.CharField(max_length=100, blank=True, null=True)
 
